137209be2990f37f78e907532a65e63f21e412e5
TaitiSamuele <samuele.taiti@gmail.com>
2025-11-21 00:25:08 +0100

    scheletro di TodoListManger

--- ToDoListManager.h
+++ ToDoListManager.h
@@ -5,9 +5,27 @@
 #ifndef TODOLISTMANAGER_H
 #define TODOLISTMANAGER_H
 
+#include "ToDoList.h"
+#include <vector>
+#include <string>
+using namespace std;
 
+//in questa classe gestirò più liste di ToDoElement, ognuna con un proprio file di salvataggio,
 
 class ToDoListManager {
+private:
+    vector<ToDoList> lists;
+    string file = "lists.txt";
+public:
+    ToDoListManager() = default;
+    ~ToDoListManager() = default;
+
+    bool menu();
+    bool getListsFromFile();
+    bool saveListsToFile();
+    bool removeListByFileName(const string& fileName);
+    bool addList(const string& fileName);
+    bool selectList(const string& fileName);
 
 };
 
